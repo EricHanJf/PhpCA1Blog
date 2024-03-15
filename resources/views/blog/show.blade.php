1180dd1bb4f2d63265638d7b97f395c81c9526fd
EricHanJf <119952942+EricHanJf@users.noreply.github.com>
2024-03-15 00:34:49 +0000

    updated some blogs and changed some css for blog page

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -1,27 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="w-4/5 m-auto text-left">
+<div class="w-4/5 mx-auto text-left">
     <div class="py-15">
-        <h1 class="text-6xl">
+        <h1 class="text-6xl font-bold text-gray-800 mt-8">
             {{ $post->title }}
         </h1>
     </div>
 </div>
 
-<div class="w-4/5 m-auto pt-20">
-    <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
-        {{ $post->blogtitle }}
-    </p>
+<div class="w-4/5 mx-auto pt-10">
+    <div class="flex items-center justify-between">
+        <p class="text-xl text-gray-500 font-bold uppercase text-shadow-md">
+            {{ $post->blogtitle }}
+        </p>
+        <span class="text-gray-500 text-sm">
+            By <span class="font-bold italic">{{ $post->user->name }}</span>, Created on {{ date('jS M Y', strtotime($post->updated_at)) }}
+        </span>
+    </div>
 
-    <img src="{{ asset('images/' . $post->image_path) }}" alt="">
-    <span class="text-gray-500">
-        By <span class="font-bold italic text-gray-800">{{ $post->user->name }}</span>, Created on {{ date('jS M Y', strtotime($post->updated_at)) }}
-    </span>
+    <img src="{{ asset('images/' . $post->image_path) }}" alt="{{ $post->title }}" class="mt-8 rounded-lg w-full blogImage mx-auto">
 
-    <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
-        {!! nl2br(e($post->description)) !!} <!-- Use nl2br() to preserve line breaks -->
-    </p>
+    
+    <div class="w-4/5 m-auto pt-8">
+        @php
+            $paragraphs = explode("\n", $post->description);
+        @endphp
+    
+        @foreach ($paragraphs as $paragraph)
+            <p class="text-lg text-gray-700 leading-8 pb-4">{{ $paragraph }}</p>
+        @endforeach
+    </div>
 </div>
 
 @endsection
